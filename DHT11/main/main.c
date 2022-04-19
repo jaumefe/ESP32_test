a68da18c54b80ec08b583aaa7db79bbe6b47fa72
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-04-19 19:28:19 +0200

    Non functional version

--- DHT11/main/main.c
+++ DHT11/main/main.c
@@ -2,8 +2,9 @@
 #include <rom/ets_sys.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-
+#include "esp_log.h"
 #define DHT11 5
+static const char* TAG = "DHT11";
 
 void startup(){
     gpio_set_level(DHT11, 0);
@@ -19,12 +20,12 @@ void configGPIO(){
         .intr_type = GPIO_INTR_DISABLE,
         .pin_bit_mask = (1ULL << DHT11),
     };
-    gpio_config(&configGPIO);
+    gpio_config(&DHT);
 }
 
 int waitOrTimeout (uint16_t us, int level){
     uint16_t timer = 0;
-    while(gpio_get_level(DHT)==level){
+    while(gpio_get_level(DHT11)==level){
         if (timer++ > us){
             timer = 99;
         }
@@ -35,12 +36,25 @@ int waitOrTimeout (uint16_t us, int level){
 
 int app_main(){
     //preparation for data
+    int data[5] = {0,0,0,0,0};
+    configGPIO();
+    vTaskDelay(500/portTICK_RATE_MS);
+    ets_delay_us(1);
+    vTaskDelay(500/portTICK_RATE_MS);
+    ets_delay_us(1);
+    vTaskDelay(500/portTICK_RATE_MS);
+    ets_delay_us(1);
+    vTaskDelay(500/portTICK_RATE_MS);
+    startup();
     int timer1 = waitOrTimeout(80, 0);
     int timer2 = waitOrTimeout(80, 1);
     //Start to transmit 1 bit
     for(int i = 0; i < 40; i++){
-        waitOrTimeout(50, 0);
-        int timer3 = waitOrTimeout(26, 1);
+        int timer3 = waitOrTimeout(50, 0);
+        if(waitOrTimeout(26, 1) > 28){
+            data[i/8] = (1 << (7 - i%8));
+        }
     }
-
+    ESP_LOGW(TAG, "Temperature:%d.%d, Humidity: %d.%d", data[0], data[1], data[2], data[3]);
+    return 0;
 }
